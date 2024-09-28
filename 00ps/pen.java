cb553e690c139dfe77062348270309723e1512c7
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-09-28 22:30:54 +0530

    Add files via upload

---
+++ 00ps/pen.java
@@ -0,0 +1,20 @@
+public class pen {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+
+    }
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
