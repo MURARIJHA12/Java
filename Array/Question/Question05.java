c6736a3e958dc9e4aa91ae54c73a99c47771a9fa
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-16 23:59:57 +0530

    Add files via upload

---
+++ Array/Question/Question05.java
@@ -0,0 +1,44 @@
+
+//Question 4th:Arrange the elements in decending  order if given an unsorted array.
+import java.util.Scanner;
+
+public class Question05 {
+	public static void main(String args[]) {
+		
+		Scanner src=new Scanner(System.in);
+		
+		System.out.println("Enter the Size of an arrays:");
+		
+		int n=src.nextInt();
+		
+		int Arr[]=new int[n];
+		
+		System.out.println("Enter the Element of an arrays:");
+		
+		for(int i=0;i<Arr.length;i++) {
+			
+			Arr[i]=src.nextInt();
+		}
+		System.out.print("orignal Array elemenyt are="+" ");
+		
+		for(int i=0;i<n;i++) {
+			
+			System.out.println(Arr[i]+" ");
+			
+		}
+		System.out.println("Decending order element:");
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(Arr[i]<Arr[j]) {
+					int temp=Arr[j];
+					Arr[j]=Arr[i];
+					Arr[i]=temp;
+				}
+			}
+			System.out.print(Arr[i]+" ");
+		}
+		
+		
+	}
+
+}
