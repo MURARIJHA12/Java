3e404eff2363b9a1806c374e70cec8c78d62e189
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-25 22:12:07 +0530

    Add files via upload

---
+++ Strings/Question/Question04.java
@@ -0,0 +1,17 @@
+package Assignement;
+//Palindrome(sentence).
+import java.util.*;
+public class Question04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner src=new Scanner(System.in);
+    System.out.println("Enter the String:");
+    String str=src.nextLine();
+    String str1=src.nextLine();
+    //str1=str.reverse();
+    
+    
+	}
+
+}
