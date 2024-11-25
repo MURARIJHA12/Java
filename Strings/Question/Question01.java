bd2c9005ede4dae44bb8892e4bf0dcc956d9eaa7
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-25 22:09:55 +0530

    Add files via upload

---
+++ Strings/Question/Question01.java
@@ -0,0 +1,19 @@
+
+//Find the length of the string.
+import java.util.Scanner;
+public class Question01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner src=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str=src.nextLine();
+		System.out.println();
+		int Length=str.length();
+		System.out.print("Length of the string"+str+"are"+Length);
+		
+		
+	}
+
+}
