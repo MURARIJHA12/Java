13b66a4742f7162d428089e339b84640f42d260a
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-25 22:11:36 +0530

    Add files via upload

---
+++ Strings/Question/Question03.java
@@ -0,0 +1,31 @@
+package Assignement;
+//  Palindrome(word).
+import java.util.*;
+public class Question03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner src=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str=src.nextLine().toLowerCase();
+		//String str2;
+		int flag = 1;
+		int first=0,last=str.length()-1;
+		while(first<last) {
+			if(str.charAt(first)!=str.charAt(last)) {
+				flag = 0;
+				break;
+			first++;
+			last--;
+		}
+		//System.out.println(flag);
+		if(flag==1) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not palindrome");
+		}
+	}
+
+}
+}
