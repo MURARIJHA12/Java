1377971d18f87d0eac93b3f76b04b78fca41d0c4
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2025-02-19 23:57:11 +0530

    Update RemoveDuplication.java

--- Top 200 question for placement/String/RemoveDuplication.java
+++ Top 200 question for placement/String/RemoveDuplication.java
@@ -1,57 +1,35 @@
 import java.util.*;
 
-class RemoveDuplication {
+class Main {
     public static void main(String args[]) {
         Scanner src = new Scanner(System.in);
-        System.out.println("Enter the String:");
+        System.out.println("Enter the String");
         String str = src.nextLine();
-        System.out.println("Original String: " + str);
-        //1st:Method
-        StringBuilder sb1=new StringBuilder();
-        str.chars().distinct().forEach(c->sb1.append((char)c));
-        System.out.println(sb1);
-        
-        //2nd methods
-        StringBuilder sb2=new StringBuilder();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            int idx=str.indexOf(ch,i+1);
-            if(idx==-1){
-                sb2.append(ch);
-            }
-        }
-        System.out.print(sb2);
-        System.out.println();
-        
-        //3rd Approch -> convert arrays and removed
-        
-        char arr[]=str.toCharArray();
-        StringBuilder sb3=new StringBuilder();
-        for(int i=0;i<arr.length;i++){
-            boolean repated=false;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    repated=true;
+        src.close();  
+        char arr[] = str.toCharArray();
+        int n = str.length();
+        int k = 0;
+        char arr1[] = new char[n];  
+
+        for (int i = 0; i < n; i++) {
+            boolean IsDuplication = false;
+            
+            for (int j = 0; j < k; j++) {  
+                if (arr[i] == arr1[j]) {  
+                    IsDuplication = true;
                     break;
                 }
             }
-            if(!repated){
-                sb3.append(arr[i]);
+
+            
+            if (!IsDuplication) {
+                arr1[k] = arr[i];
+                k++;
             }
         }
-        System.out.println(sb3);
-        
-       StringBuilder sb4 = new StringBuilder();
-        Set<Character> set = new LinkedHashSet<>();
-        
-        for (int i = 0; i < str.length(); i++) {
-            set.add(str.charAt(i)); 
+        System.out.println("String after removing duplicate characters:");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr1[i]);
         }
-        
-        for (Character c : set) {
-            sb4.append(c); 
-        }
-        
-        System.out.println("String with unique characters: " + sb4);
     }
 }
